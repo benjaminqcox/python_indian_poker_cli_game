d0e0cc0c27e87795e1ffe35f41fff900e48b0dc4
benjaminqcox <benjaminqcox@gmail.com>
2022-08-09 18:13:14 +0100

    Refactored program to have methods correctly placed in their class. Used same method for bot and player to reduce repeating. There may be some improvements that can be made, but I am ready to move onto the visuals.

--- deck.py
+++ deck.py
@@ -2,7 +2,7 @@ from card import Card
 from random import shuffle
 
 SUITS = ['C', 'H', 'S', 'D']
-VALUE = ['A', '2', 'K']
+VALUE = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
 
 class Deck:
