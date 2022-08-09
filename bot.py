e9726279cb544de774ed7e98b239cb423e914b7e
benjaminqcox <benjaminqcox@gmail.com>
2022-08-09 18:12:56 +0100

    Refactored program to have methods correctly placed in their class. Used same method for bot and player to reduce repeating. There may be some improvements that can be made, but I am ready to move onto the visuals.

--- bot.py
+++ bot.py
@@ -1,6 +1,19 @@
+from random import randint
+from typing import List
+
+from card import Card
 from player import Player
 
 class Bot(Player):
-    pass
+    def choose_card_indexes(self, row: str, centre_card: Card) -> List[int]:
+        if row == 'face_down':
+            return [randint(0, len(self.cards[row])-1)]
+        bot_selection = self.bot_card_selection(row, centre_card)
+        return self.all_indexes_matching_chosen_card(row, bot_selection)
 
-    # Need to add bot ability to play then the game is done
+    def bot_card_selection(self, row, centre_card):
+        has_magic = self.cards[row][0].is_magic()
+        for card_index in range(len(self.cards[row])):
+            if not self.cards[row][card_index].is_magic() and self.has_chosen_legal_card(self.cards[row][card_index], centre_card):
+                return card_index
+        return randint(0, len(self.cards[row])-1) if not has_magic else 0
